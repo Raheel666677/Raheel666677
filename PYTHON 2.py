c53a7697b402b4ba1416a7d70231643675622ac9
Raheel666677 <153253861+Raheel666677@users.noreply.github.com>
2024-01-05 08:37:29 +0500

    Add files via upload

---
+++ PYTHON 2.py
@@ -0,0 +1,15 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hello/Downloads/pythone codes/PYTHON 1.py
+Enter the base of the triangle: 120
+Enter the height of the triangle: 20
+The area of the triangle with base 120.0 and height 20.0 is: 1200.0
+>>> 
+========== RESTART: C:/Users/hello/Downloads/pythone codes/PYTHON 1.py =========
+Enter the base of the triangle: 50
+Enter the height of the triangle: 79
+The area of the triangle with base 50.0 and height 79.0 is: 1975.0
+Enter the first floating-point number: 123
+Enter the second floating-point number: 45
+The average of 123.00 and 45.00 is: 84.00
